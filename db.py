3881ab24ab0009a544d98375a884a13ed1d40456
Jocelyn Shek <jocelynshek@dhcp-10-250-137-176.harvard.edu>
2023-11-28 00:59:29 -0500

    Updated db.py

--- db.py
+++ db.py
@@ -1,1 +1,20 @@
+import pandas as pd
+import sqlite3
 
+# Read CSV file
+df = pd.read_csv('penguin_info.csv')
+
+# Connect to SQLite DB
+conn = sqlite3.connect('penguins.db')
+df.to_sql('penguins', conn, if_exists='replace', index=False)
+conn.close()
+
+@app.route('/search')
+def search_penguins():
+    query = request.args.get('query')
+    conn = sqlite3.connect('penguins.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM penguins WHERE species LIKE ?", ('%' + query + '%',))
+    results = cursor.fetchall()
+    conn.close()
+    return jsonify(results)
