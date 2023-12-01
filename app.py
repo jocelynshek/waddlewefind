df93b811def679dc9b9feee387c6da00cafd7524
Kinguin8 <ricardofernandesgarcia@college.harvard.edu>
2023-11-30 21:06:37 -0500

    suffering and testin flask run part 2

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
-print('hi')
+from flask import Flask, render_template
 
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    # Renders the index.html template
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
