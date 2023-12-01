001e6bd833fb4086e286f137bb2fffeac22d7e40
Kinguin8 <ricardofernandesgarcia@college.harvard.edu>
2023-11-30 23:49:38 -0500

    suffering and testin flask run part 9

--- static/js/main.js
+++ static/js/main.js
@@ -1,18 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const titleScreen = document.getElementById('title-screen');
-    const startButton = document.getElementById('start-button');
+    
+    // Function starting game when Enter key pressed
+    function startGameOnEnter(event) {
+        if (event.key === "Enter") {
+            // Redirect to letter html starting game
+            window.location.href = "letter.html";
+        }
 
-    // Function to handle starting the game
-    function startGame() {
-        // You can add code here to start your game:
-        // For example, hide the title screen and show the game section
-        titleScreen.style.display = 'none';
-        // Example: document.getElementById('game-section').style.display = 'block';
-        
-        // Additional game initialization code can be added here
-        // ...
     }
 
     // Event listener for the Start button click
-    startButton.addEventListener('click', startGame);
+    document.addEventListener('keydown', startGameOnEnter);
 });
