467f45a5ea8871dd16f4657e2d93f58fa8fa9d7e
Kinguin8 <ricardofernandesgarcia@college.harvard.edu>
2023-11-30 22:03:04 -0500

    suffering and testin flask run part 7

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,18 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const titleScreen = document.getElementById('title-screen');
+    const startButton = document.getElementById('start-button');
 
+    // Function to handle starting the game
+    function startGame() {
+        // You can add code here to start your game:
+        // For example, hide the title screen and show the game section
+        titleScreen.style.display = 'none';
+        // Example: document.getElementById('game-section').style.display = 'block';
+        
+        // Additional game initialization code can be added here
+        // ...
+    }
+
+    // Event listener for the Start button click
+    startButton.addEventListener('click', startGame);
+});
