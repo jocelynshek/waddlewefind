40d8df0011a52bd7506542a853c72d088abd8eca
Jocelyn Shek <jocelynshek@dhcp-10-250-137-176.harvard.edu>
2023-12-03 16:36:32 -0500

    reorganizing ng and cleaning up the doc

--- app.py
+++ app.py
@@ -20,15 +20,8 @@ def computer():
     # Render computer.html page
     return render_template('computer.html')
 
-
-
 @app.route('/search.html')
 def search():
-    #Route to serve the search page.
-    #rows = db.execute("SELECT cash FROM users WHERE id = :id", id=session["user_id"])
-    #if not rows:
-        #return apology("missing user")
-    # Query the database for the first dropdown data
     categories = ["Species", "Origin", "Location", "Sex", "Age"]
     
     # Render the template with the categories data
@@ -40,45 +33,7 @@ def get_options():
     print("SELECT DISTINCT " + category + " FROM penguins")
     options = db.execute("SELECT DISTINCT " + category + " FROM penguins")
     print(options)
-    return jsonify(options) 
-
-
-
-"""@app.route('/get_options')
-def get_options():
-    category = request.args.get('category')
-    # Query the database based on the selected category
-    options = db.execute("SELECT option FROM penguins WHERE category = ?", [category])
-    
-    # Return the options as JSON
-    return jsonify([o[0] for o in options])"""
-
-
-
-
-"""@app.route('/get_options')
-def getOptions():
-    #Route to get options based on the selected category.
-    category = request.args.get('category')
-
-    valid_categories = ['Species', 'Origin', 'Location', 'Sex', 'Age']  # Update with your actual column names
-    if category not in valid_categories:
-        return jsonify({"error": "Invalid category"}), 400
-
-    query = f"SELECT DISTINCT {category} FROM penguins"
-
-
-    options = query_db(query)
-    return jsonify(options=[o[0] for o in options])
-
-@app.route('/get_penguins')
-def get_penguins():
-    #Route to get penguins based on the selected category and option.
-    category = request.args.get('category')
-    option = request.args.get('option')
-    query = f"SELECT * FROM penguins WHERE {category} = ?"
-    penguins = query_db(query, [option])
-    return jsonify(penguins=[dict(zip(['name', 'species', 'origin', 'location', 'sex', 'age'], p)) for p in penguins])"""
+    return jsonify(options)
 
 #@app.route('/check')
 #def check():
